f6c12932d602f38033954f5fc9268b6e6a7ea8d0
TayuyaGate <83429510+TayuyaGate@users.noreply.github.com>
2021-06-15 07:27:51 -0300

    Update Calculadora.java

--- src/main/java/br/ufmg/dcc/Calculadora.java
+++ src/main/java/br/ufmg/dcc/Calculadora.java
@@ -1,9 +1,11 @@
+--- a/src/main/java/br/ufmg/dcc/Calculadora.java
++++ b/src/main/java/br/ufmg/dcc/Calculadora.java
 package br.ufmg.dcc;
 
 public class Calculadora {
 
   public int soma(int x, int y) {
-    return x + y;
+    return x + y + 1;
   }
 
   public int subtrai(int x, int y) {
